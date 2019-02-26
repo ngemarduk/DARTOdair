fec439c719c767078fca1f1d64d06f470edaf339
Odair José dos Santos Junior <odairsj@hotmail.com>
2019-02-26 20:50:04 -0300

    Integração do MVC com a camada de serviço.

---
+++ DARTOdairMVC/Controllers/DinheiroController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DARTOdairMVC.Models;
+using DinheiroService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DARTOdairMVC.Controllers
+{
+    public class DinheiroController : Controller
+    {
+        // GET: Dinheiro
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        // POST: Dinheiro/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(DinheiroModel dinheiro)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    DinheirCalculaService objDinheiroCalcula = new DinheirCalculaService();
+
+                    var retorno = objDinheiroCalcula.DinheiroCalcula(dinheiro.valorTotal, dinheiro.valorPago);
+
+                }
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+       
+       
+    }
+}
---
+++ DARTOdairMVC/Models/DinheiroModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DARTOdairMVC.Models
+{
+    public class DinheiroModel
+    {
+        [Required(ErrorMessage = "Digite o valor total.")]
+        [DataType(DataType.Currency)]
+        public decimal valorTotal { get; set;}
+
+        [Required(ErrorMessage = "Digite o valor pago.")]
+        [DataType(DataType.Currency)]
+        public decimal valorPago { get; set; }
+
+    }
+}
